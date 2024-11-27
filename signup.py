009a700993f8642e9d9d13e11b8bf1360f065e7b
NandhanNA17 <nandhan.p@kriyadocs.com>
2024-11-27 17:05:20 +0530

    New updated

--- signup.py
+++ signup.py
@@ -1,6 +1,7 @@
 # 1. Define a function to greet the user
 def greet(name):
     print(f"Hello, {name}!")
+    print(f"Hello, {name}!") # Makeing Duplicated
 
 # 2. Input from the user
 name = input("Enter your name: ")
@@ -10,7 +11,7 @@ greet(name)
 
 # 4. Define a function to calculate the square of a number
 def square(number):
-    return number * number
+    return number * name
 
 # 5. Input for number
 number = int(input("Enter a number to square: "))
